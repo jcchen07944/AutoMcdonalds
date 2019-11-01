712acfcf8cef7d47ba9f781ec2aa920d415bd6f9
JCChen <james35022000@gmail.com>
2019-11-02 02:34:23 +0800

    Remove unuse code.

--- encrypter/src/Main.java
+++ encrypter/src/Main.java
@@ -6,6 +6,7 @@ import io.github.jcchen07944031.Entities.Coupon;
 public class Main {
 
 	public static void main(String[] argv) {
+<<<<<<< HEAD
 		Account account = new Account("", "");
 		account.login();
 		account.verifyAccessToken();
@@ -14,6 +15,19 @@ public class Main {
 		
 		//System.out.println(Encrypt.encode(null, "9Android SDK built for x86_64e43b4f1d26bcae412019/10/02 00:00:002.2.112345678901234567890e43b4f1d26bcae4120191002000000000Android"));
 		//System.out.println(Encrypt.encode(context.get, (new String("12345")).toString()));
+=======
+		Database database = new Database();
+		
+		ArrayList<Account> accountList = database.getAccounts();
+		for(int i = 0; i < accountList.size(); i++) {
+			Account account = accountList.get(i);
+			account.login();
+			database.updateAccount(account);
+
+			Coupon coupon = new Coupon(account);
+			coupon.getLottery();
+		}
+>>>>>>> e6e60f3... Remove unuse code.
 	}
 
 	
