b862d364b282f13b51439599c90b7a3358c7c453
JCChen <james35022000@gmail.com>
2019-11-03 08:43:35 +0800

    Finish register page(communicate with backend not yet).

---
+++ Frontend/js/main.js
@@ -0,0 +1,41 @@
+;(function() {
+
+	'use strict';
+	
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+	
+	var fullHeight = function() {
+
+			if ( !isMobile.any() ) {
+				$('.js-fullheight').css('height', $(window).height());
+				$(window).resize(function(){
+					$('.js-fullheight').css('height', $(window).height());
+				});
+			}
+
+	};
+	
+	$(function(){
+			fullHeight();
+	});
+	
+}());
